c5f0efc0a142d0e54a7c93b1bf1f1266063b2fbf
niuge <694838286@qq.com>
2022-07-03 22:46:35 +0800

    update

--- js-Interview/手写call,bind,apply.js
+++ js-Interview/手写call,bind,apply.js
@@ -72,4 +72,22 @@ Function.prototype.call = function(context) {
     }
  }
  
+ //手写apply
+  Function.prototype.apply = function(context, args) {
+    // 判断调用对象
+    if (typeof this !== "function") {
+      console.error("type error");
+    }
+    // 获取参数
+    let result = null;
+    // 判断 context 是否传入，如果未传入则设置为 window
+    context = context || window;
+    // 将调用函数设为对象的方法
+    context.fn = this;
+    // 调用函数
+    result = context.fn(...args);
+    // 将属性删除
+    delete context.fn;
+    return result;
+  }
  
