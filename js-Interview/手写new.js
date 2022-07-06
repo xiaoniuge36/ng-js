1fa95e6f9cfb4af15ba49913decb27c9fb203999
niuge <694838286@qq.com>
2022-07-07 00:24:29 +0800

    update

--- js-Interview/手写new.js
+++ js-Interview/手写new.js
@@ -27,15 +27,31 @@ function myNew(fn, ...args) {
         Person.prototype.say = function() {
             console.log(this.age);
         }
-        let p1 = myNew(Person, "lihua", 18);
-        console.log(p1.name);
-        console.log(p1);
-        p1.say();
-        console.log(p1.__proto__);
-        console.log(Person.prototype);
-        console.log(Person.prototype.__proto__);
-        console.log(Person.prototype.__proto__.__proto__);
+        // let p1 = myNew(Person, "lihua", 18);
+        // console.log(p1.name);
+        // console.log(p1);
+        // p1.say();
+        // console.log(p1.__proto__);
+        // console.log(Person.prototype);
+        // console.log(Person.prototype.__proto__);
+        // console.log(Person.prototype.__proto__.__proto__);
 
+//手写柯里化
+function curry(fn) {
+  // 将具有length属性的对象转换成数组
+    let args = [].slice.call(arguments, 1);
+    return function() {
+        let newArgs = args.concat([].slice.call(arguments));
+        return fn.apply(this, newArgs);
+    }
+}
+//   用法如下：
+  function add(a, b) {
+    return a + b;
+  }
+  let add10 = curry(add, 10);
+  console.log(add10(20));
+ 
 
 
 
